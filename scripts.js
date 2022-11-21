02bc30d5f656a1c246b1b828abd43e010201c812
0sakib0 <88287970+0sakib0@users.noreply.github.com>
2022-11-21 13:36:23 +0600

    Add single round of Rock Paper Scissors

--- scripts.js
+++ scripts.js
@@ -3,3 +3,21 @@ const getComputerChoice = () => {
     return arrays[Math.floor(Math.random() * arrays.length)];
 };
 console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You win! Rock beats Scissors";
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You win! Paper beats Rock";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You win! Scissors beats Paper";
+    } else if (playerSelection == computerSelection) {
+        return `It's a Tie! You both picked ${playerSelection}`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
