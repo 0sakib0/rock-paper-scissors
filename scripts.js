065049053954015cb78af1946bf06dc779c7ba9d
0sakib0 <88287970+0sakib0@users.noreply.github.com>
2022-11-21 23:50:21 +0600

    Add 5 round of Rock Paper Scissors

--- scripts.js
+++ scripts.js
@@ -2,22 +2,59 @@ const getComputerChoice = () => {
     let arrays = ["rock","paper","scissors"];
     return arrays[Math.floor(Math.random() * arrays.length)];
 };
-console.log(getComputerChoice());
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "rock" && computerSelection == "scissors") {
-        return "You win! Rock beats Scissors";
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        return "You win! Paper beats Rock";
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "You win! Scissors beats Paper";
-    } else if (playerSelection == computerSelection) {
-        return `It's a Tie! You both picked ${playerSelection}`;
+
+function winnerChecker(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "Tie"; 
+    } else if (
+    (playerSelection == "rock" && computerSelection == "scissors") || 
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper")
+    ) {
+        return "Player";
     } else {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    }
+        return "Computer";
+    }  
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function playRound(playerSelection, computerSelection) {
+    const result = winnerChecker(playerSelection, computerSelection);
+    if(result == "Tie") {
+        return `It's a Tie! You both picked ${playerSelection}`
+    } else if (result == "Player") {
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection};`
+    }
+  }
+
+function getPlayerChoice() {
+        const playerChoice = prompt("Enter your move between Rock Paper and Scissors:");
+        return playerChoice.toLowerCase();
+}
+
+function game() {
+    let userScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        
+        let playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        if (winnerChecker(playerSelection, computerSelection) == "Computer") {
+            ++computerScore;
+    } else if(winnerChecker(playerSelection, computerSelection) == "Player") {
+        ++userScore;
+    }
+
+} console.log("Game Over")
+  if(userScore > computerScore) {
+    console.log("You won!");
+  } else {
+    console.log("Computer Won!");
+  }
+
+}
+
+game();
